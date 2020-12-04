3ee826175d163b0b80b68a36e50bcc567d359bda
Your Name <you@example.com>
2020-12-04 10:28:53 +0700

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipesComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
